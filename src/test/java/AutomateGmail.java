f4177c39821d81d69430b1d6da6ca5bf606794f9
itichausali2 <itichausali@rediffmail.com>
2017-04-06 16:37:02 +0530

    modified automationtest

--- src/test/java/AutomateGmail.java
+++ src/test/java/AutomateGmail.java
@@ -4,18 +4,36 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class AutomateGmail {
-	
-	WebDriver driver;
-	@BeforeTest
-	public void launchBrowser() {
-		 System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"//Drivers//geckodriver.exe");
-		 driver = new FirefoxDriver();
-		 driver.get("https://www.google.co.in/");
+public class AutomateGmail {public void launchBrowser() {
+		System.setProperty("webdriver.gecko.driver", "D://Workspace//TestGmail//Drivers//geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.get("https://www.google.co.in/");
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 	}
+
 	@Test
 	public void startupScreen() {
-		driver.findElement(By.xpath(".//*[@id='sb_ifc0']")).sendKeys("Gmail");
+	
+
+		WebElement element = driver.findElement(By.xpath(".//*[@id='sb_ifc0']"));
+		element.sendKeys("Gmail");
+
+		element.sendKeys(Keys.ENTER);
+
+		driver.findElement(By.linkText("Gmail - Google")).click();
+		driver.findElement(By.id("Email")).sendKeys("itichausali");
+		driver.findElement(By.id("next")).click();
+		WebElement password = driver.findElement(By.id("Passwd"));
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.visibilityOf(password)).isDisplayed();
+		driver.findElement(By.id("Passwd")).sendKeys("###messup");
+		driver.findElement(By.id("signIn")).sendKeys(Keys.ENTER);
+
+	}
+
+	@Test
+	public void composeMail() {
+			driver.findElement(By.linkText("COMPOSE")).click();
 		
 	}
 }
